db7154ca0af31f7673a8b858ecf8171944d310e7
hasanalanya <alanya.hasan@outlook.com>
2020-08-31 22:20:34 +0200

    adding new function

--- printtable.py
+++ printtable.py
@@ -6,5 +6,17 @@ def multtable(start, stop, number):
     for i in range(start, stop+1):
         print(f"{i} x {number} = {i*number}")
 
-if __name__ == '__main__':
     multtable(1, 4, 7)
+
+ def powertable(power, number):
+     """
+     Print the powertable for <number^power>. 
+     Print this output for every <number^power>.
+     """
+
+     for i in range(1, number+1):
+         print(f"{i} ^{power} = {i**power}")
+
+ if __name__ == '__main__':
+     powertable(2, 4)
+     powertable(3, 3)
